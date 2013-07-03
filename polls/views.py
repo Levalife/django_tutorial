be1eb2cc2ecf373012ac4a992eebb1660a33e712
levushka14@gmail.com <levushka14@gmail.com>
2013-07-03 16:46:13 +0300

    part 5, testing

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from polls.models import Poll, Choice
 
@@ -11,7 +12,8 @@ class IndexView(generic.ListView):
 	context_object_name = 'latest_poll_list'
 
 	def get_queryset(self):
-		return Poll.objects.order_by('-pub_date')[:5]
+		return Poll.objects.filter(pub_date__lte=timezone.now()
+		).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
 	model = Poll
